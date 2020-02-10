550ef2d3240c97f8f6d7eae9296c0696dbd715e7
hynn <heyjuu1208@gmail.com>
2020-02-10 16:19:05 +0900

    Hi branch 추가함

--- Hello/src/Hello.java
+++ Hello/src/Hello.java
@@ -2,10 +2,10 @@
 public class Hello {
 	public static void main(String[] args) {
 		System.out.println("Hello");
-		System.out.printIn("Hello Java");
+		System.out.println("Hello Java");
 		System.out.println("Hi-master");
-		System.out.printIn("Hi Java");
-
+		System.out.println("Hi-Java");
+		System.out.println("Hi-branch");
 	}
 
 }
